f3de4f58baed0760058e69b1003653ccf8ed7611
genialnish <natalia.ustimenko93@gmail.com>
2021-02-25 16:36:18 -0800

    running on wikipedia pug page

--- src/background.ts
+++ src/background.ts
@@ -2,3 +2,9 @@ console.log('BACKGROUND SCRIPT: Hello World')
 
 
 const word = document.querySelector('')
+
+chrome.tabs.getCurrent(tab => {
+    alert(tab.title);
+    find('Wikipedia', 'Pediwikia', document.body)
+
+});
